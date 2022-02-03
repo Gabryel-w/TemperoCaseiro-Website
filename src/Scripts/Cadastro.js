c056c4bbd39c6496792f055fdba7c9d998cd4069
Gabryel Willers <gabryelqwer@gmail.com>
2022-02-03 13:52:01 -0300

    Segundo Commit

--- src/Scripts/Cadastro.js
+++ src/Scripts/Cadastro.js
@@ -6,10 +6,13 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+
+ 
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCpf(input)
 }
 
 function validaDataNascimento(input) {
@@ -28,4 +31,79 @@ function maiorQue18(data) {
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
+}
+
+//Validação de Cpf//
+
+function validaCpf(input){
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checarCpfRepetido(cpfFormatado) || !checaEstruturaCpf(cpfFormatado) ){
+        mensagem = 'O Cpf Digitado não é valido'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checarCpfRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    let cpfValido = true
+
+    valoresRepetidos.forEach(valor =>{
+        if(valor == cpf){
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+
+function checaEstruturaCpf(cpf){
+    const multiplicador = 10
+
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12){
+        return true
+    }
+    
+    
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11)
 }
